17ba7cfa2c9bc2cab7e6d2a7578c0dc05523aab5
Dark Sun <dacom.dark.sun@gmail.com>
2022-06-23 18:24:44 +0300

    fix

--- bots/vac/index.js
+++ bots/vac/index.js
@@ -410,7 +410,7 @@ module.exports.init = async (botModel, bot) => {
           //SEND FROM USER IN BOT TO PUB CHANNEL
           // console.log("\n\non here2")
           if (user.state === 'chat') {
-            // console.log("try to send: ", bot.getEnv().CHAT_CHANNEL, 'reply_to: ', user.resume_chat_id)
+            console.log("try to send: ", bot.getEnv().CHAT_CHANNEL, 'reply_to: ', user.resume_chat_id)
             const id = await sendMessageToUser(bot, { id: bot.getEnv().CHAT_CHANNEL }, { text }, {reply_to_message_id : user.resume_chat_id});
 
             await insertMessage(bot.instanceName, user, bot.getEnv().CHAT_CHANNEL, text, id, 'chat');
@@ -425,18 +425,19 @@ module.exports.init = async (botModel, bot) => {
       }
     } else {
       if (ctx.update.message && ctx.update.message.is_automatic_forward == true && ctx.update.message.sender_chat){
-            if (ctx.update.message.sender_chat.id == bot.getEnv().CV_CHANNEL){ //если словили пересылку из прикрепленного канала
-              if(ctx.update.message.forward_from_chat.id == bot.getEnv().CV_CHANNEL){ //то нужно запомнить ID сообщения, чтоб отвечать в том же треде
-                user = await getUserByResumeChannelId(bot.instanceName, ctx.update.message.forward_from_message_id)
+          if (ctx.update.message.sender_chat.id == bot.getEnv().CV_CHANNEL){ //если словили пересылку из прикрепленного канала
+            if(ctx.update.message.forward_from_chat.id == bot.getEnv().CV_CHANNEL){ //то нужно запомнить ID сообщения, чтоб отвечать в том же треде
+              user = await getUserByResumeChannelId(bot.instanceName, ctx.update.message.forward_from_message_id)
 
-                if (user && !user.resume_chat_id){
-                  user.resume_chat_id = ctx.update.message.message_id
-                  await saveUser(bot.instanceName, user);  
-                }
-                
+              if (user && !user.resume_chat_id){
+                console.log("catch forwarded messsage to chat: ", ctx.update.message.message_id)
+                user.resume_chat_id = ctx.update.message.message_id
+                await saveUser(bot.instanceName, user);  
               }
+              
             }
-          } else { //Или отправляем пользователю ответ в личку если это ответ на резюме пользователя
+          }
+        } else { //Или отправляем пользователю ответ в личку если это ответ на резюме пользователя
       }
     }
   });
