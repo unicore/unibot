2ea5faec9003e9416483f2914e689235909f71bf
Dark Sun <dacom.dark.sun@gmail.com>
2022-12-01 15:28:07 +0300

    fix

--- bots/knouni/messages.js
+++ bots/knouni/messages.js
@@ -5,23 +5,32 @@ const { loadDB } = require('./db');
 
 async function sendMessageToUser(bot, user, message, extra) {
   let id = {};
-  if ('text' in message) id = await bot.telegram.sendMessage(user.id, message.text, extra);
-  if ('photo' in message) id = await bot.telegram.sendPhoto(user.id, message.photo[3].file_id);
-  if ('voice' in message) id = await bot.telegram.sendVoice(user.id, message.voice.file_id);
-  if ('video_note' in message) id = await bot.telegram.sendVideoNote(user.id, message.video_note.file_id);
-  if ('video' in message) id = await bot.telegram.sendVideo(user.id, message.video.file_id);
-  if ('doc' in message) {
-    // eslint-disable-next-line max-len
-    id = await bot.telegram.sendDocument(user.id, { source: message.doc, filename: extra.filename });
-  } if ('venue' in message) {
-    // eslint-disable-next-line max-len
-    id = await bot.telegram.sendVenue(user.id, message.location.latitude, message.location.longitude, message.venue.title, message.venue.address);
-  } else if ('location' in message) {
-    // eslint-disable-next-line max-len
-    id = await bot.telegram.sendLocation(user.id, message.location.latitude, message.location.longitude);
-  }
+  
+  try {
 
-  return id.message_id;
+    if ('text' in message) id = await bot.telegram.sendMessage(user.id, message.text, extra);
+    if ('photo' in message) id = await bot.telegram.sendPhoto(user.id, message.photo[3].file_id);
+    if ('voice' in message) id = await bot.telegram.sendVoice(user.id, message.voice.file_id);
+    if ('video_note' in message) id = await bot.telegram.sendVideoNote(user.id, message.video_note.file_id);
+    if ('video' in message) id = await bot.telegram.sendVideo(user.id, message.video.file_id);
+    if ('doc' in message) {
+      // eslint-disable-next-line max-len
+      id = await bot.telegram.sendDocument(user.id, { source: message.doc, filename: extra.filename });
+    } if ('venue' in message) {
+      // eslint-disable-next-line max-len
+      id = await bot.telegram.sendVenue(user.id, message.location.latitude, message.location.longitude, message.venue.title, message.venue.address);
+    } else if ('location' in message) {
+      // eslint-disable-next-line max-len
+      id = await bot.telegram.sendLocation(user.id, message.location.latitude, message.location.longitude);
+    }
+  
+    return id.message_id;
+  
+  } catch(e) {
+    console.log("error on send: ", e.message)
+    return null
+  } 
+  
 }
 
 async function sendMessageToAll(bot, message, extra) {
